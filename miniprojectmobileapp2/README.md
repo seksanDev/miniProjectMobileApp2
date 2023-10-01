c09b84936c6cae7edb602ecaa944c63c40ce7565
seksan_dev <seksan.plan@gmail.com>
2023-10-01 16:17:26 +0700

    first commit

---
+++ miniprojectmobileapp2/README.md
@@ -0,0 +1,3 @@
+# miniprojectmobileapp2
+
+A new Flutter project.
---
+++ miniprojectmobileapp2/android/app/src/main/kotlin/com/example/miniprojectmobileapp2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.miniprojectmobileapp2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
